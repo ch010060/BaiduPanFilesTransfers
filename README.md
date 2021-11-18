e2646c2dd8dab538d35c323c1f33d9c7941afba4
ch010060 <ch010060@gmail.com>
2021-11-18 21:15:54 +0800

    Fixed batch transfer error(200025).

--- README.md
+++ README.md
@@ -1,119 +1,119 @@
 BaiduPanFilesTransfers
 -----
-#### 介绍使用
+#### 介紹使用
 
-百度网盘批量转存工具,基于Python 3.8+Tkinter
-详细介绍使用请访问:[小众软件](https://meta.appinn.net/t/topic/16995/39)
+百度網盤批量轉存工具,基於Python 3.8+Tkinter
+詳細介紹使用請訪問:[小眾軟體](https://meta.appinn.net/t/topic/16995/39)
 
-![1.9版本截图](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/%E6%88%AA%E5%9B%BE1.9.jpg)
+![1.9版本截圖](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/%E6%88%AA%E5%9B%BE1.9.jpg)
 
-**1.获取Cookie和User-Agent:**
+**1.獲取Cookie和User-Agent:**
 
-使用Chrome或类似浏览器访问百度网盘主页,完全载入后按F12调出控制台,选择网络(NetWork)选项卡,目前应该空空如也:
-![向导图1](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/u-1.png)
-按F5刷新页面,下面出现很多条记录,点击home那条右边会出现菜单,显示标头(Headers),响应(Response)等内容.看标头里往下翻找到Cookie项目,后面有一串以BAIDUID开头的长长内容,这就是需要找的Cookies了,把它们选中全部复制出来粘贴到软件对应输入框.
-![向导图2](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/u-2.png)
-再往下翻能看到User-Agent项目,同样把它复制粘贴到软件对应输入框便行了.软件会自动保存当前配置,下次无需再次操作.
-![向导图3](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/u-3.png)
+使用Chrome或類似瀏覽器訪問百度網盤主頁,完全載入后按F12調出控制檯,選擇網路(NetWork)選項卡,目前應該空空如也:
+![嚮導圖1](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/u-1.png)
+按F5重新整理頁面,下面出現很多條記錄,點選home那條右邊會出現菜單,顯示標頭(Headers),響應(Response)等內容.看標頭裡往下翻找到Cookie專案,後面有一串以BAIDUID開頭的長長內容,這就是需要找的Cookies了,把它們選中全部複製出來貼上到軟體對應輸入框.
+![嚮導圖2](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/u-2.png)
+再往下翻能看到User-Agent專案,同樣把它複製貼上到軟體對應輸入框便行了.軟體會自動儲存目前配置,下次無需再次操作.
+![嚮導圖3](https://raw.githubusercontent.com/hxz393/BaiduPanFilesTransfers/master/Capture/u-3.png)
 
-**2.输入保存目标和网盘链接:**
+**2.輸入儲存目標和網盤鏈接:**
 
-保存目录如果不输入则保存到根目录下,保存目录不存在会自动新建.
+儲存目錄如果不輸入則儲存到根目錄下,儲存目錄不存在會自動新建.
 
-链接支持格式:
+鏈接支援格式:
 
-*标准链接*
+*標準鏈接*
 ```
-https://pan.baidu.com/s/1EFCrmlh0rhnWy8pi9uhkyA 提取码：4444
+https://pan.baidu.com/s/1EFCrmlh0rhnWy8pi9uhkyA 提取碼：4444
 https://pan.baidu.com/s/14Az6kqaluwtUDr5JH9WViA 提取:v70q
 https://pan.baidu.com/s/1nvBwS25lENYceUu3OMH4tg 6img
 https://pan.baidu.com/s/1EFCrmlh0rhnWy8pi9uhkyA
 https://pan.baidu.com/share/init?surl=W7U9g47xiDez_5ItgNIs0w
 ```
 
-*游侠 v1标准*
+*遊俠 v1標準*
 ```
 https://pan.baidu.com/#bdlink=QkQyMTUxNjJENzE5NDc4QkNBRDJGMTMyNTlFMTEzNzAjRkJBMTEzQTY1M0QxN0Q1NjM3QUQ1MEEzRTgwMkE2QTIjMzcxOTgxOTIzI1pha3VybyAyMDAxMjYuN3oK
 bdlink=QkQyMTUxNjJENzE5NDc4QkNBRDJGMTMyNTlFMTEzNzAjRkJBMTEzQTY1M0QxN0Q1NjM3QUQ1MEEzRTgwMkE2QTIjMzcxOTgxOTIzI1pha3VybyAyMDAxMjYuN3oK
 ```
 
-*梦姬标准*
+*夢姬標準*
 ```
-965FEAFCC6DC216CB56128B531694C9D#495B4FB5879AE0B22A31826D33D86D80#802846691#梦姬标准.7z
+965FEAFCC6DC216CB56128B531694C9D#495B4FB5879AE0B22A31826D33D86D80#802846691#夢姬標準.7z
 ```
 
 *Go格式*
 ```
-BaiduPCS-Go rapidupload -length=418024594 -md5=31f141fee63d038a46db179367315f3a -slicemd5=5b2c842f421143a9a49938dc157c52e6 -crc32=3179342807 \"/音乐/Yes/1969. Yes.zip\"
+BaiduPCS-Go rapidupload -length=418024594 -md5=31f141fee63d038a46db179367315f3a -slicemd5=5b2c842f421143a9a49938dc157c52e6 -crc32=3179342807 \"/音樂/Yes/1969. Yes.zip\"
 ```
 
-*PanDL标准*
+*PanDL標準*
 ```
 bdpan://44K344Or44Kv44Gu5p6c5a6fICsg44Go44KJ44Gu44GC44Gq44CA5o+P44GN5LiL44KN44GXOFDlsI/lhorlrZAg5pel5paHLnppcHw2NDAxODQxNTd8ZDNjOTBmOTI3ZjUxYzIyMmRjMTc1NDM1YTY0OWMyYTJ8OTk4NTE0NDE3Y2I5Y2I0MTQ0MGRlZTFiMmMyNTYwMzY=`
 ```
 
-**3.注意事项:**
+**3.注意事項:**
 
-1.如在浏览器端登出百度账号,再次登录需要重新手动获取Cookie值,否则会提示获取不到bdstoken;
+1.如在瀏覽器端登出百度賬號,再次登錄需要重新手動獲取Cookie值,否則會提示獲取不到bdstoken;
 
-2.同一账号在多浏览器登录会导致获取不到shareid.请退出所有账号并重新打开唯一的浏览器登录(建议使用Chrome)并获取Cookie和User-Agent.
+2.同一賬號在多瀏覽器登錄會導致獲取不到shareid.請退出所有賬號並重新打開唯一的瀏覽器登錄(建議使用Chrome)並獲取Cookie和User-Agent.
 _ _ _
-#### 下载使用
-点到release页面下载bpftUI.exe,直接打开使用
+#### 下載使用
+點到release頁面下載bpftUI.exe,直接打開使用
 _ _ _
 #### 自行打包
-1. 克隆项目,在CMD中切换至项目目录
+1. 克隆專案,在CMD中切換至專案目錄
 2. 使用pyinstaller打包:
 ``
 pyinstaller -F -w -i bpftUI.ico bpftUI.py
 ``
 _ _ _
-#### 更新日志
+#### 更新日誌
 **2020.05.09 更新版本 1.9**
 
-1.修复可用性;
+1.修復可用性;
 
-2.增加对旧式生成链接的支持;
+2.增加對舊式產生鏈接的支援;
 
-3.增加cookie非法字符检测.
+3.增加cookie非法字元檢測.
 
 **2020.12.15 更新版本 1.8**
 
-修复可用性,修改部分用语
+修復可用性,修改部分用語
 
 **2020.11.01 更新版本 1.7**
 
-更进百度更新,修改链接验证逻辑
+更進百度更新,修改鏈接驗證邏輯
 
 **2020.09.14 更新版本 1.6**
 
-修复无验证码分享文件夹链接转存提示'验证码错误'问题
+修復無驗證碼分享資料夾鏈接轉存提示'驗證碼錯誤'問題
 
 **2020.08.15 更新版本 1.5**
 
-1.修复非文件多文件共享链接只转存第一个文件的问题;
+1.修復非檔案多檔案共享鏈接只轉存第一個檔案的問題;
 
-2.增加User-Agent输入框以应对百度网盘新一轮更新;
+2.增加User-Agent輸入框以應對百度網盤新一輪更新;
 
-3.增加配置保存功能.
+3.增加配置儲存功能.
 
 **2020.08.12 更新版本 1.4**
 
-修复百度更新后获取不到bdstoken问题
+修復百度更新后獲取不到bdstoken問題
 
 **2020.07.26 更新版本 1.3**
 
-修复部分秒传链接无法识别问题
+修復部分秒傳鏈接無法識別問題
 
 **2020.07.07 更新版本 1.2**
 
-修复由于md5值大小写原因造成的秒传链接转存失败
+修復由於md5值大小寫原因造成的秒傳鏈接轉存失敗
 
 **2020.06.19 更新版本 1.1**
 
-增加对提取码不正确,文件已被删除,弹出验证码状态判断
+增加對提取碼不正確,檔案已被刪除,彈出驗證碼狀態判斷
 
 **2020.06.16 更新版本 1.0**
 
-增加对文件夹和秒传链接的支持
+增加對資料夾和秒傳鏈接的支援
 
--- bpftUI.py
+++ bpftUI.py
@@ -14,41 +14,41 @@ import urllib3
 from retrying import retry
 
 '''
-软件名: BaiduPanFilesTransfers
+軟體名: BaiduPanFilesTransfers
 版本: 1.9
-更新时间: 2021.05.09
+更新時間: 2021.05.09
 打包命令: pyinstaller -F -w -i bpftUI.ico bpftUI.py
 '''
 
-# 实例化TK
+# 實例化TK
 root = Tk()
 
-# 运行时替换图标
+# 執行時替換圖示
 ICON = zlib.decompress(base64.b64decode('eJxjYGAEQgEBBiDJwZDBysAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc='))
 _, ICON_PATH = tempfile.mkstemp()
 with open(ICON_PATH, 'wb') as icon_file:
     icon_file.write(ICON)
 root.iconbitmap(default=ICON_PATH)
 
-# 主窗口配置
-root.wm_title("度盘转存 1.9 by Alice & Asu")
+# 主視窗配置
+root.wm_title("度盤轉存 2.0 by Alice & Asu & Roger")
 root.wm_geometry('350x473+240+240')
 root.wm_attributes("-alpha", 0.91)
 root.resizable(width=False, height=False)
 
-# 定义标签和文本框
-Label(root, text='1.下面填入百度Cookies,不带引号').grid(row=1, column=0, sticky=W)
+# 定義標籤和文字框
+Label(root, text='1.下面填入百度Cookies,不帶引號').grid(row=1, column=0, sticky=W)
 entry_cookie = Entry(root, width=48, )
 entry_cookie.grid(row=2, column=0, sticky=W, padx=4)
-Label(root, text='2.下面填入浏览器User-Agent').grid(row=3, column=0, sticky=W)
+Label(root, text='2.下面填入瀏覽器User-Agent').grid(row=3, column=0, sticky=W)
 entry_ua = Entry(root, width=48, )
 entry_ua.grid(row=4, column=0, sticky=W, padx=4)
-Label(root, text='3.下面填入文件保存位置(默认根目录),不能包含<,>,|,*,?,,/').grid(row=5, column=0, sticky=W)
+Label(root, text='3.下面填入檔案儲存位置(預設根目錄),不能包含<,>,|,*,?,,/').grid(row=5, column=0, sticky=W)
 entry_folder_name = Entry(root, width=48, )
 entry_folder_name.grid(row=6, column=0, sticky=W, padx=4)
-Label(root, text='4.下面粘贴链接,每行一个,格式为:链接 提取码.支持秒传格式.').grid(row=7, sticky=W)
+Label(root, text='4.下面貼上鏈接,每行一個,格式為:鏈接 提取碼.支援秒傳格式.').grid(row=7, sticky=W)
 
-# 链接输入框
+# 鏈接輸入框
 text_links = Text(root, width=48, height=10, wrap=NONE)
 text_links.grid(row=8, column=0, sticky=W, padx=4, )
 scrollbar_links = Scrollbar(root, width=5)
@@ -56,7 +56,7 @@ scrollbar_links.grid(row=8, column=0, sticky=S + N + E, )
 scrollbar_links.configure(command=text_links.yview)
 text_links.configure(yscrollcommand=scrollbar_links.set)
 
-# 日志输出框
+# 日誌輸出框
 text_logs = Text(root, width=48, height=10, wrap=NONE)
 text_logs.grid(row=10, column=0, sticky=W, padx=4, )
 scrollbar_logs = Scrollbar(root, width=5)
@@ -64,21 +64,21 @@ scrollbar_logs.grid(row=10, column=0, sticky=S + N + E, )
 scrollbar_logs.configure(command=text_logs.yview)
 text_logs.configure(yscrollcommand=scrollbar_logs.set)
 
-# 定义按钮和状态标签
-bottom_run = Button(root, text='4.点击运行', command=lambda: thread_it(main, ), width=10, height=1, relief='solid')
+# 定義按鈕和狀態標籤
+bottom_run = Button(root, text='4.點選執行', command=lambda: thread_it(main, ), width=10, height=1, relief='solid')
 bottom_run.grid(row=9, pady=6, sticky=W, padx=4)
-label_state = Label(root, text='检查新版', font=('Arial', 9, 'underline'), foreground="#0000ff", cursor='heart')
+label_state = Label(root, text='檢查新版', font=('Arial', 9, 'underline'), foreground="#0000ff", cursor='heart')
 label_state.grid(row=9, sticky=E, padx=4)
 label_state.bind("<Button-1>", lambda e: webbrowser.open("https://github.com/hxz393/BaiduPanFilesTransfers", new=0))
 
-# 读取配置
+# 讀取配置
 if os.path.exists('config.ini'):
     with open('config.ini') as config_read:
         [config_cookie, config_user_agent] = config_read.readlines()
     entry_cookie.insert(0, config_cookie)
     entry_ua.insert(0, config_user_agent)
 
-# 公共请求头
+# 公共請求頭
 request_header = {
     'Host': 'pan.baidu.com',
     'Connection': 'keep-alive',
@@ -95,7 +95,7 @@ urllib3.disable_warnings()
 s = requests.session()
 
 
-# 获取bdstoken函数
+# 獲取bdstoken函式
 @retry(stop_max_attempt_number=5, wait_fixed=1000)
 def get_bdstoken():
     url = 'https://pan.baidu.com/disk/home'
@@ -105,7 +105,7 @@ def get_bdstoken():
     return bdstoken_list[0] if bdstoken_list else 1
 
 
-# 获取目录列表函数
+# 獲取目錄列表函式
 @retry(stop_max_attempt_number=5, wait_fixed=1000)
 def get_dir_list(bdstoken):
     url = 'https://pan.baidu.com/api/list?order=time&desc=1&showempty=0&web=1&page=1&num=1000&dir=%2F&bdstoken=' + bdstoken
@@ -113,7 +113,7 @@ def get_dir_list(bdstoken):
     return response.json()['errno'] if response.json()['errno'] != 0 else response.json()['list']
 
 
-# 新建目录函数
+# 新建目錄函式
 @retry(stop_max_attempt_number=5, wait_fixed=1000)
 def create_dir(dir_name, bdstoken):
     url = 'https://pan.baidu.com/api/create?a=commit&bdstoken=' + bdstoken
@@ -122,7 +122,7 @@ def create_dir(dir_name, bdstoken):
     return response.json()['errno']
 
 
-# 检测链接种类
+# 檢測鏈接種類
 def check_link_type(link_list_line):
     if link_list_line.find('https://pan.baidu.com/s/') >= 0:
         link_type = '/s/'
@@ -135,18 +135,18 @@ def check_link_type(link_list_line):
     return link_type
 
 
-# 验证链接函数
+# 驗證鏈接函式
 @retry(stop_max_attempt_number=20, wait_fixed=2000)
 def check_links(link_url, pass_code, bdstoken):
-    # 验证提取码
+    # 驗證提取碼
     if pass_code:
-        # 生成时间戳
+        # 產生時間戳
         t_str = str(int(round(time.time() * 1000)))
         check_url = 'https://pan.baidu.com/share/verify?surl=' + link_url[25:48] + '&bdstoken=' + bdstoken + '&t=' + t_str + '&channel=chunlei&web=1&clienttype=0'
         post_data = {'pwd': pass_code, 'vcode': '', 'vcode_str': '', }
         response_post = s.post(url=check_url, headers=request_header, data=post_data, timeout=10, allow_redirects=False,
                                verify=False)
-        # 在cookie中加入bdclnd参数
+        # 在cookie中加入bdclnd參數
         if response_post.json()['errno'] == 0:
             bdclnd = response_post.json()['randsk']
         else:
@@ -155,7 +155,7 @@ def check_links(link_url, pass_code, bdstoken):
             request_header['Cookie'] = re.sub(r'BDCLND=(\S+?);', r'BDCLND=' + bdclnd + ';', request_header['Cookie'])
         else:
             request_header['Cookie'] += '; BDCLND=' + bdclnd
-    # 获取文件信息
+    # 獲取檔案資訊
     response = s.get(url=link_url, headers=request_header, timeout=15, allow_redirects=True,
                      verify=False).content.decode("utf-8")
     shareid_list = re.findall('"shareid":(\\d+?),"', response)
@@ -171,7 +171,7 @@ def check_links(link_url, pass_code, bdstoken):
         return [shareid_list[0], user_id_list[0], fs_id_list]
 
 
-# 转存文件函数
+# 轉存檔案函式
 @retry(stop_max_attempt_number=200, wait_fixed=2000)
 def transfer_files(check_links_reason, dir_name, bdstoken):
     url = 'https://pan.baidu.com/share/transfer?shareid=' + check_links_reason[0] + '&from=' + check_links_reason[
@@ -183,7 +183,7 @@ def transfer_files(check_links_reason, dir_name, bdstoken):
     return response.json()
 
 
-# 转存秒传链接函数
+# 轉存秒傳鏈接函式
 @retry(stop_max_attempt_number=100, wait_fixed=1000)
 def transfer_files_rapid(rapid_data, dir_name, bdstoken):
     url = 'https://pan.baidu.com/api/rapidupload?bdstoken=' + bdstoken
@@ -198,30 +198,40 @@ def transfer_files_rapid(rapid_data, dir_name, bdstoken):
     return response.json()['errno']
 
 
-# 状态标签变化函数
+# 狀態標籤變化函式
 def label_state_change(state, task_count=0, task_total_count=0):
     label_state.unbind("<Button-1>")
     label_state['font'] = ('Arial', 9)
     label_state['foreground'] = "#000000"
     label_state['cursor'] = "arrow"
     if state == 'error':
-        label_state['text'] = '发生错误,错误日志如下:'
+        label_state['text'] = '發生錯誤,錯誤日誌如下:'
     elif state == 'running':
-        label_state['text'] = '下面为转存结果,进度:' + str(task_count) + '/' + str(task_total_count)
+        label_state['text'] = '下面為轉存結果,進度:' + str(task_count) + '/' + str(task_total_count)
 
 
-# 多线程
+# 多執行緒
 def thread_it(func, *args):
     t = threading.Thread(target=func, args=args)
     t.setDaemon(True)
     t.start()
     # t.join()
 
-
-# 主程序
+# modified main
 def main():
-    # 获取和初始化数据
-    text_logs.delete(1.0, END)
+    # modify text_input
+    text_logs.insert(END, '=== 轉存開始 ===' + '\n')
+    text_input = text_links.get(1.0, END).split('\n')
+    link_list = [link for link in text_input if link]
+    link_list = [link + ' ' for link in link_list]
+    for i, link in enumerate(link_list):
+        text_logs.insert(END, f'({i+1}/{len(link_list)})' + '\n')
+        _main([link])
+
+
+# 主程式
+def _main(link_list):
+    # 獲取和初始化數據
     dir_name = "".join(entry_folder_name.get().split())
     cookie = "".join(entry_cookie.get().split())
     request_header['Cookie'] = cookie
@@ -229,102 +239,100 @@ def main():
     request_header['User-Agent'] = user_agent
     with open('config.ini', 'w') as config_write:
         config_write.write(cookie + '\n' + user_agent)
-    text_input = text_links.get(1.0, END).split('\n')
-    link_list = [link for link in text_input if link]
-    link_list = [link + ' ' for link in link_list]
+
     task_count = 0
     task_total_count = len(link_list)
     bottom_run['state'] = 'disabled'
     bottom_run['relief'] = 'groove'
-    bottom_run['text'] = '运行中...'
+    bottom_run['text'] = '執行中...'
 
-    # 开始运行函数
+    # 開始執行函式
     try:
-        # 检查cookie输入是否正确
+        # 檢查cookie輸入是否正確
         if cookie.find('BAIDUID=') == -1:
             label_state_change(state='error')
-            text_logs.insert(END, '百度网盘cookie输入不正确,请检查cookie后重试.' + '\n')
+            text_logs.insert(END, '百度網盤cookie輸入不正確,請檢查cookie後重試.' + '\n')
             sys.exit()
         
         if any([ord(word) not in range(256) for word in cookie]):
             label_state_change(state='error')
-            text_logs.insert(END, '百度网盘cookie中带非法字符,请检查cookie后重试.' + '\n')
+            text_logs.insert(END, '百度網盤cookie中帶非法字元,請檢查cookie後重試.' + '\n')
             sys.exit()
 
-        # 执行获取bdstoken
+        # 執行獲取bdstoken
         bdstoken = get_bdstoken()
         if bdstoken == 1:
             label_state_change(state='error')
-            text_logs.insert(END, '没获取到bdstoken,请检查cookie和网络后重试.' + '\n')
+            text_logs.insert(END, '沒獲取到bdstoken,請檢查cookie和網路後重試.' + '\n')
             sys.exit()
 
-        # 执行获取目录列表
+        # 執行獲取目錄列表
         dir_list_json = get_dir_list(bdstoken)
         if type(dir_list_json) != list:
             label_state_change(state='error')
-            text_logs.insert(END, '没获取到网盘目录列表,请检查cookie和网络后重试.' + '\n')
+            text_logs.insert(END, '沒獲取到網盤目錄列表,請檢查cookie和網路後重試.' + '\n')
             sys.exit()
 
-        # 执行新建目录
+        # 執行新建目錄
         dir_list = [dir_json['server_filename'] for dir_json in dir_list_json]
         if dir_name and dir_name not in dir_list:
             create_dir_reason = create_dir(dir_name, bdstoken)
             if create_dir_reason != 0:
                 label_state_change(state='error')
-                text_logs.insert(END, '文件夹名带非法字符,请改正文件夹名称后重试.' + '\n')
+                text_logs.insert(END, '資料夾名帶非法字元,請改正資料夾名稱後重試.' + '\n')
                 sys.exit()
 
-        # 执行转存
+        # 執行轉存
         for url_code in link_list:
-            # 处理旧格式链接
+            # 處理舊格式鏈接
             url_code = url_code.replace("https://pan.baidu.com/share/init?surl=", "https://pan.baidu.com/s/1")
-            # 判断连接类型
+            # 判斷連線型別
             link_type = check_link_type(url_code)
-            # 处理(https://pan.baidu.com/s/1tU58ChMSPmx4e3-kDx1mLg lice)格式链接
+            # 處理(https://pan.baidu.com/s/1tU58ChMSPmx4e3-kDx1mLg lice)格式鏈接
             if link_type == '/s/':
-                link_url, pass_code = re.sub(r'提取码*[：:](.*)', r'\1', url_code.lstrip()).split(' ', maxsplit=1)
+                link_url, pass_code = re.sub(r'提取碼*[：:](.*)', r'\1', url_code.lstrip()).split(' ', maxsplit=1)
                 pass_code = pass_code.strip()[:4]
-                # 执行检查链接有效性
+                # 執行檢查鏈接有效性
                 check_links_reason = check_links(link_url, pass_code, bdstoken)
                 if check_links_reason == 1:
-                    text_logs.insert(END, '链接失效,没获取到shareid:' + url_code + '\n')
+                    text_logs.insert(END, '鏈接失效,沒獲取到shareid:' + url_code + '\n')
                 elif check_links_reason == 2:
-                    text_logs.insert(END, '链接失效,没获取到user_id:' + url_code + '\n')
+                    text_logs.insert(END, '鏈接失效,沒獲取到user_id:' + url_code + '\n')
                 elif check_links_reason == 3 or check_links_reason == -9:
-                    text_logs.insert(END, '链接失效,文件已经被删除或取消分享:' + url_code + '\n')
+                    text_logs.insert(END, '鏈接失效,檔案已經被刪除或取消分享:' + url_code + '\n')
                 elif check_links_reason == -12:
-                    text_logs.insert(END, '提取码错误:' + url_code + '\n')
+                    text_logs.insert(END, '提取碼錯誤:' + url_code + '\n')
                 elif check_links_reason == -62:
-                    text_logs.insert(END, '错误尝试次数过多,请稍后再试:' + url_code + '\n')
+                    text_logs.insert(END, '錯誤嘗試次數過多,請稍後再試:' + url_code + '\n')
                 elif isinstance(check_links_reason, list):
-                    # 执行转存文件
+                    # 執行轉存檔案
                     transfer_files_reason = transfer_files(check_links_reason, dir_name, bdstoken)
                     if transfer_files_reason['errno'] == 0:
-                        text_logs.insert(END, '转存成功:' + url_code + '\n')
+                        text_logs.insert(END, '轉存成功:' + url_code + '\n')
                     elif transfer_files_reason['errno'] == 12 and transfer_files_reason['info'][0]['errno'] == -30:
-                        text_logs.insert(END, '转存失败,目录中已有同名文件存在:' + url_code + '\n')
+                        text_logs.insert(END, '轉存失敗,目錄中已有同名檔案存在:' + url_code + '\n')
                     elif transfer_files_reason['errno'] == 12 and transfer_files_reason['info'][0]['errno'] == 120:
-                        text_logs.insert(END, '转存失败,转存文件数超过限制:' + url_code + '\n')
+                        text_logs.insert(END, '轉存失敗,轉存檔案數超過限制:' + url_code + '\n')
                     else:
                         text_logs.insert(END,
-                                         '转存失败,错误代码(' + str(transfer_files_reason['errno']) + '):' + url_code + '\n')
+                                         '轉存失敗,錯誤程式碼(' + str(transfer_files_reason['errno']) + '):' + url_code + '\n')
                 else:
-                    text_logs.insert(END, '访问链接返回错误代码(' + str(check_links_reason) + '):' + url_code + '\n')
-            # 处理秒传格式链接
+                    text_logs.insert(END, '訪問鏈接返回錯誤程式碼(' + str(check_links_reason) + '):' + url_code + '\n')
+            # 處理秒傳格式鏈接
             elif link_type == 'rapid':
-                # 处理梦姬标准(4FFB5BC751CC3B7A354436F85FF865EE#797B1FFF9526F8B5759663EC0460F40E#21247774#秒传.rar)
+                # 處理夢姬標準(4FFB5BC751CC3B7A354436F85FF865EE#797B1FFF9526F8B5759663EC0460F40E#21247774#秒傳.rar)
                 if url_code.count('#') > 2:
                     rapid_data = url_code.split('#', maxsplit=3)
-                # 处理游侠 v1标准(bdlink=)
+                # 處理遊俠 v1標準(bdlink=)
                 elif bool(re.search('bdlink=', url_code, re.IGNORECASE)):
                     rapid_data = base64.b64decode(re.findall(r'bdlink=(.+)', url_code)[0]).decode(
                         "utf-8").strip().split('#', maxsplit=3)
-                # 处理PanDL标准(bdpan://)
+                # 處理PanDL標準(bdpan://)
                 elif bool(re.search('bdpan://', url_code, re.IGNORECASE)):
                     bdpan_data = base64.b64decode(re.findall(r'bdpan://(.+)', url_code)[0]).decode(
                         "utf-8").strip().split('|')
                     rapid_data = [bdpan_data[2], bdpan_data[3], bdpan_data[1], bdpan_data[0]]
-                # 处理PCS-Go标准(BaiduPCS-Go)
+                # 處理PCS-Go標準(BaiduPCS-Go)
                 elif bool(re.search('BaiduPCS-Go', url_code, re.IGNORECASE)):
                     go_md5 = re.findall(r'-md5=(\S+)', url_code)[0]
                     go_md5s = re.findall(r'-slicemd5=(\S+)', url_code)[0]
@@ -335,37 +343,37 @@ def main():
                     rapid_data = []
                 transfer_files_reason = transfer_files_rapid(rapid_data, dir_name, bdstoken)
                 if transfer_files_reason == 0:
-                    text_logs.insert(END, '转存成功:' + url_code + '\n')
+                    text_logs.insert(END, '轉存成功:' + url_code + '\n')
                 elif transfer_files_reason == -8:
-                    text_logs.insert(END, '转存失败,目录中已有同名文件存在:' + url_code + '\n')
+                    text_logs.insert(END, '轉存失敗,目錄中已有同名檔案存在:' + url_code + '\n')
                 elif transfer_files_reason == 404:
-                    text_logs.insert(END, '转存失败,秒传无效:' + url_code + '\n')
+                    text_logs.insert(END, '轉存失敗,秒傳無效:' + url_code + '\n')
                 elif transfer_files_reason == 2:
-                    text_logs.insert(END, '转存失败,非法路径:' + url_code + '\n')
+                    text_logs.insert(END, '轉存失敗,非法路徑:' + url_code + '\n')
                 elif transfer_files_reason == -7:
-                    text_logs.insert(END, '转存失败,非法文件名:' + url_code + '\n')
+                    text_logs.insert(END, '轉存失敗,非法檔名:' + url_code + '\n')
                 elif transfer_files_reason == -10:
-                    text_logs.insert(END, '转存失败,容量不足:' + url_code + '\n')
+                    text_logs.insert(END, '轉存失敗,容量不足:' + url_code + '\n')
                 elif transfer_files_reason == 114514:
-                    text_logs.insert(END, '转存失败,接口调用失败:' + url_code + '\n')
+                    text_logs.insert(END, '轉存失敗,介面呼叫失敗:' + url_code + '\n')
                 else:
-                    text_logs.insert(END, '转存失败,错误代码(' + str(transfer_files_reason) + '):' + url_code + '\n')
+                    text_logs.insert(END, '轉存失敗,錯誤程式碼(' + str(transfer_files_reason) + '):' + url_code + '\n')
             elif link_type == 'unknown':
-                text_logs.insert(END, '不支持链接:' + url_code + '\n')
+                text_logs.insert(END, '不支援鏈接:' + url_code + '\n')
             task_count = task_count + 1
             label_state_change(state='running', task_count=task_count, task_total_count=task_total_count)
     except Exception as e:
-        text_logs.insert(END, '运行出错,请重新运行本程序.错误信息如下:' + '\n')
+        text_logs.insert(END, '執行出錯,請重新執行本程式.錯誤資訊如下:' + '\n')
         text_logs.insert(END, str(e) + '\n\n')
-        text_logs.insert(END, '用户输入内容:' + '\n')
+        text_logs.insert(END, '使用者輸入內容:' + '\n')
         text_logs.insert(END, '百度Cookies:' + cookie + '\n')
-        text_logs.insert(END, '文件夹名:' + dir_name + '\n')
-        text_logs.insert(END, '链接输入:' + '\n' + str(text_input))
-    # 恢复按钮状态
+        text_logs.insert(END, '資料夾名:' + dir_name + '\n')
+        text_logs.insert(END, '鏈接輸入:' + '\n' + str(text_input))
+    # 恢復按鈕狀態
     finally:
         bottom_run['state'] = 'normal'
         bottom_run['relief'] = 'solid'
-        bottom_run['text'] = '4.点击运行'
+        bottom_run['text'] = '4.點選執行'
 
 
 root.mainloop()
